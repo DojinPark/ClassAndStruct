c4c1035a259c1a786108eb5ae852f68135002c57
Dojin Park <dojin.git@gmail.com>
2023-08-20 03:53:43 +0900

    init

---
+++ ClassAndStruct/ClassAndStructApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassAndStructApp.swift
+//  ClassAndStruct
+//
+//  Created by Dojin Park on 2023/08/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassAndStructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
